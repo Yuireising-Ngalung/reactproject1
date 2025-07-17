1dc10608566b9d6c9d34c0acf2f7eccb21f187bb
Yuireising <yuireisingngalung2000@gmail.com>
2025-07-17 23:57:04 +0530

    Home Page updated

--- src/pages/home/HomeIndexPage.jsx
+++ src/pages/home/HomeIndexPage.jsx
@@ -2,7 +2,7 @@ import { useReducer, useState } from "react";
 import HomeUI from "./HomeUI";
 import { initialValues, reducer } from "./todo.action";
 
-export default function(){
+export default function HomeIndexPage(){
     const [newTodo, setNewTodo] = useState('');
 
     const [todos, dispatch] = useReducer(reducer, initialValues);
