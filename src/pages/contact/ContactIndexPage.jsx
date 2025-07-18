72949d3484fce361d6444d4dd49a197e7ba228fc
Yuireising <yuireisingngalung2000@gmail.com>
2025-07-18 18:37:25 +0530

    contactus form UI

--- src/pages/contact/ContactIndexPage.jsx
+++ src/pages/contact/ContactIndexPage.jsx
@@ -1,26 +1,26 @@
 export default function ContactIndexPage(){
     return(<>
         <div className="container my-5">
-            <h2 className="text-primary">CONTACT PAGE</h2>
+            <h2>CONTACT PAGE</h2>
             <hr />
-            <section className="shadow p-5 rounded">
+            <section className="shadow p-5 rounded shadow border-top border-5 border-primary p-5 py-4 px-4">
                 <form>
-                    <div className="form-group mt-3">
+                    <div className="form-group mb-3">
                         <label htmlFor="name" className="fw-bold">Name</label>
                         <input type="text" id="name" name="name" placeholder="enter username" className="form-control"/>
                     </div> 
 
-                    <div className="form-group mt-3">
+                    <div className="form-group mb-3">
                         <label htmlFor="email" className="fw-bold">Email</label>
                         <input type="email" id="email" name="email" placeholder="enter email id" className="form-control" />
                     </div>
 
-                    <div className="form-group mt-3">
+                    <div className="form-group mb-3">
                         <label htmlFor="message" className="fw-bold">Message</label>
                         <textarea name="message" id="message" className="form-control" placeholder="enter your message"></textarea>
                     </div>
 
-                    <div className="d-flex gap-3 mt-3">
+                    <div className="d-flex gap-3">
                         <button className="btn btn-primary text-uppercase">send message</button>
                         <button className="btn btn-danger text-uppercase">cancel</button>
                     </div>
